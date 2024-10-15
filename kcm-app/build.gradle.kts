69de3c9e157e747e4cbc69a9ce1eb9bd7cd5ae5f
rainy <rainy@luoyuer.com>
2024-10-15 10:31:44 +0800

    Remove ShadowJar

--- kcm-app/build.gradle.kts
+++ kcm-app/build.gradle.kts
@@ -2,7 +2,6 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("application")
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
@@ -31,11 +30,6 @@ tasks.test {
 kotlin {
     jvmToolchain(8)
 }
-tasks.withType<ShadowJar>{
-    manifest {
-        attributes(
-            "Manifest-Version" to 1.0,
-            "Main-Class" to "com.luoyuer.MainKt"
-        )
-    }
+application{
+    mainClass = "com.luoyuer.MainKt"
 }
